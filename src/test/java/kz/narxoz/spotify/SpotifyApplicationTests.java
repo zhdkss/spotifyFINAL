98b40ce0d25ea646806432744c5ca2b4cc345c2c
Dias <zhakaevdias228@gmail.com>
2025-12-18 16:18:36 +0500

    Default Proj

---
+++ src/test/java/kz/narxoz/spotify/SpotifyApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.narxoz.spotify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
